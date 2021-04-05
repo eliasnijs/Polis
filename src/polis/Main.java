f20c8aabeaa5fe40fa129788d360afc2ea69176c
eliasnijs <elias.nijs@gmail.com>
2021-04-05 13:46:49 +0200

    1. startmenu completed

--- src/polis/Main.java
+++ src/polis/Main.java
@@ -17,40 +17,28 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     private static final String TITLE = "Polis - A city builder";
-    private static final String FXML_LOCATION = "/polis/main.fxml";
+    private static final String FXML_GAME = "/polis/main.fxml";
+    private static final String FXML_START = "/polis/startmenu.fxml";
 
 
     @Override
     public void start(Stage stage) throws Exception {
 
-        Label l = new Label("POLIS");
-        l.setStyle("-fx-text-fill: #ffffffff; -fx-font-size: 72px");
-        Button b = new Button("Press me!!!");
-        b.setStyle("-fx-background-color: ddddddff; -fx-text-fill: black;");
-        VBox v = new VBox(l,b);
-        v.setAlignment(Pos.CENTER);
-        v.setSpacing(30);
-        Label l2 = new Label("" +
-                "Programming - Elias Nijs\n" +
-                "Asset Design - Elias Nijs\n" +
-                "Music - HeatleyBros");
-        l2.setAlignment(Pos.CENTER);
-        VBox v2 = new VBox(v,l2);
-        v2.setSpacing(60);
-        v2.setAlignment(Pos.CENTER);
-        l2.setStyle("-fx-text-fill: e2e2e2; -fx-alignment: center");
 
-        StackPane pane = new StackPane(v2);
-        Scene start = new Scene(pane, 1600,685);
-        start.getStylesheets().add("/polis/main.css");
+        FXMLLoader start = new FXMLLoader(getClass().getResource(FXML_START));
+        Parent startParent = start.load();
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource(FXML_LOCATION));
-        Parent root = loader.load();
-        Scene game = new Scene(root, 1600,685);
+        FXMLLoader game = new FXMLLoader(getClass().getResource(FXML_GAME));
+        Parent gameParent = game.load();
 
-        b.setOnAction(e -> stage.setScene(game));
+        Scene gameScene = new Scene(gameParent, 1600,685);
 
-        stage.setScene(start);
+        startParent.setOnMouseReleased(e -> {stage.setScene(gameScene);
+            System.out.println("asd");});
+        Scene startScene = new Scene(startParent, 1600,685);
+
+
+        stage.setScene(startScene);
         stage.setTitle(TITLE);
         stage.show();
     }
